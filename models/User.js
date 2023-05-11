1d58702a64f56896f9942baac019bf977d934331
Sophia Perez <sophiaperez@MacBook-Air.attlocal.net>
2023-05-11 18:17:00 -0500

    Worked on home routes, handlebars, etc.

--- models/User.js
+++ models/User.js
@@ -1,50 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-
-    person_name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-
+    person_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [8],
-        },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
       },
-
+    },
     species_wanted: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true
     },
 
     breed: {
-        type: DataTypes.STRING,
+      type: DataTypes.STRING,
 
-    }
-
-
-    }
+    },
+  }
 );
 
 module.exports = User;
