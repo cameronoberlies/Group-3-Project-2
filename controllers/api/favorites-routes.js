20786deb8f29be225a604d721436844a674340b4
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-05-19 13:48:52 -0500

    fixed favorites and calendar issues

--- controllers/api/favorites-routes.js
+++ controllers/api/favorites-routes.js
@@ -4,9 +4,11 @@ const { userFavorites } = require('../../models');
 router.post("/:id", async(req,res) => {
     try{
       const favorite = await userFavorites.create({pets_id: req.params.id, user_id: req.session.user_id})
-      res.json(200).end()
+      console.log(favorite)
+      res.status(200).json(favorite)
     }
     catch(err) {
+      console.log(err)
       res.status(400).json(err)
     }
   })
