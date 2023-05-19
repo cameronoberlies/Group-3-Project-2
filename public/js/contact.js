befd1e53ea4e4b0507963b1f9320b7a816800e64
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-05-19 15:12:40 -0500

    contact features

---
+++ public/js/contact.js
@@ -0,0 +1,32 @@
+
+
+async function mainMail(name, email, subject, message) {
+  const transporter = await nodeMail.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const mailOption = {
+    from: process.env.GMAIL_USER,
+    to: process.env.EMAIL,
+    subject: subject,
+    html: `You got a message from 
+    Email : ${email}
+    Name: ${name}
+    Message: ${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    return Promise.resolve("Message Sent Successfully!");
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+
+
+
+
+
