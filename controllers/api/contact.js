0bfaa8ca47333e85938e9a6d5596f0e6b3b6c620
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-05-23 21:39:00 -0500

    send mail

---
+++ controllers/api/contact.js
@@ -0,0 +1,44 @@
+const nodeMail = require('nodemailer');
+const router = require('express').Router();
+
+
+
+router.post('/', async (req,res) => {
+  
+let email = {...req.body}
+
+console.log(email)
+
+  const transporter = await nodeMail.createTransport({
+    service: "gmail",
+    auth: {
+      user: "adoptapetoday@gmail.com",
+      pass: "mjlttjfuwmsgvfnw",
+    },
+  });
+  const mailOption = {
+    from: process.env.GMAIL_USER,
+    to: process.env.EMAIL,
+    subject: email.subject,
+    html: `You got a message from 
+    Email : ${email.email}
+    Name: ${email.name}
+    Message: ${email.message}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    console.log("Message Sent!")
+    return response.status(200).json("Message Sent Successfully!");
+  } catch (error) {
+    return response.status(500).json(error)
+  }
+
+});
+
+module.exports = router;
+
+
+
+
+
+
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const favoriteRoutes = require('./favorites-routes');
+const contactRoutes = require('./contact');
 
 router.use('/users', userRoutes);
 router.use('/favorites', favoriteRoutes);
+router.use('/contact', contactRoutes);
 
 module.exports = router;
